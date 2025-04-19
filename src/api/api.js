b1ad05fa8675400990dd362f82e3d2a44fdba42d
bogdan9861 <besedin_b@bk.ru>
2025-04-19 19:24:45 +0300

    change url

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://localhost:8080/api`,
-  // baseURL: `https://laws-server.onrender.com/api`,
+  // baseURL: `http://localhost:8080/api`,
+  baseURL: `https://laws-server.onrender.com/api`,
 });
 
 api.interceptors.request.use(
